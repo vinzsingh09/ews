5b922efc1e9a57fc8c865fc6cdb2d87daea4b6bd
Vineet Singh <vinzsingh09@gmail.com>
2014-03-24 12:58:57 +1200

    changes in version for upgrading in moodle

--- version.php
+++ version.php
@@ -6,8 +6,9 @@
  * @subpackage block_dashboard
  */
 
-$plugin->version  = 2014021000;
-$plugin->requires = 2013111801;
+$plugin->version  = 2014031204;
+//$plugin->requires = 2013111801;
+$plugin->requires = 2010112400;
 $plugin->maturity = 200;
 $plugin->release  = 'Version for Moodle 2.6+';
 $plugin->cron  = 7200;  // every 2hours cron
