4068837d7a89ace07b929743d1ce960414bd2426
Vineet Singh <vinzsingh09@gmail.com>
2014-03-24 12:59:22 +1200

    changes for localhost

--- push/config.php
+++ push/config.php
@@ -2,19 +2,19 @@
 	// I have given this new database for elearndev
 	// Actual implementation can be done using this or early_warning_sys DB
 	//database hostname
-	$hostname="timetablerdev.usp.ac.fj";
-	//$hostname="localhost";
+	//$hostname="timetablerdev.usp.ac.fj";
+	$hostname="localhost";
 	
 	//mysql username
-	$username="moodle";
-	//$username="root";
+	//$username="moodle";
+	$username="root";
 	
 	//mysql password
-	$password="moodle";
-	//$password="";
+	//$password="moodle";
+	$password="";
 	
 	// Database Name
-	$database="elearn";
-	//$database="earlywarningsystem1";
+	//$database="elearn";
+	$database="earlywarningsystem1";
 
 ?>
