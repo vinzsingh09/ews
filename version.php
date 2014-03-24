3ea94f130136ae64e3faeedafb02ebb95ac33013
Vineet Singh <vinzsingh09@gmail.com>
2014-03-24 12:53:47 +1200

    Not needed, its not used anywhere

--- version.php
+++ version.php
@@ -6,8 +6,8 @@
  * @subpackage block_dashboard
  */
 
-$plugin->version  = 2014011000;
-$plugin->requires = 2010112400;
+$plugin->version  = 2014021000;
+$plugin->requires = 2013111801;
 $plugin->maturity = 200;
-$plugin->release  = 'Version for Moodle 2.x';
+$plugin->release  = 'Version for Moodle 2.6+';
 $plugin->cron  = 7200;  // every 2hours cron
